e5da71dd20b931ef9401ac0ea950118abc1edd7d
Николай Мелешенко <nikolaymeleshenko@MacBook-Air-Nikolaj.local>
2025-07-31 19:57:13 +0300

    another-fix-scripts-js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite';
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/front-end-entrance-exam/',
-  root: '.',
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    open: true,
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'js'),
-    },
-  },
-  css: {
-    devSourcemap: true,
-  },
+
+  base: './',
+  plugins: [],
 });
